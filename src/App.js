3d6accc9c637cf9d8578bfc8e1770cfd187cef61
kiitan-jsx <ajaladamilola09@protonmail.com>
2023-09-25 05:58:09 -0400

    logo

--- src/App.js
+++ src/App.js
@@ -2,18 +2,29 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-gradient-to-t from-slate-50 via-zinc-50 to-zinc-100 ">
 
-      <div className='bg-white'>
-        <h1 className='mt-40 mb-40 font-rale'> <span className='text-gradient'>w</span><span className="text-gradient">e</span><span className="text-gradient">l</span><span className="text-gradient">k</span><span className="text-gradient">i</span><span className="text-gradient">n</span><span className="text-gradient"></span></h1>
-      </div> 
+      <div  style={{ height: '700%' }}>
+        <div className=' pt-40 pb-40 bg-gradient-to-b from-slate-50 via-zinc-50 to-zinc-100'>
+          <h1 className='mt-60 mb-40 font-rale'>
+            <span className='text-gradient'>w</span>
+            <span className='text-gradient'>e</span>
+            <span className='text-gradient'>l</span>
+            <span className='text-gradient'>k</span>
+            <span className='text-gradient'>i</span>
+            <span className='text-gradient'>n</span>
+            <span className='text-gradient'></span>
+          </h1>
+        </div>
+      </div>
 
 
-      <div className='bg-white'>
+
+      <div className='bg-gradient-to-b from-slate-50 via-zinc-50 to-zinc-100'>
         <h1 className='mt-20 mb-40 font-rale'> <span className='text-gradient1'>{'<'}{'>'}</span> <span className='text-gradient'>w</span><span className="text-gradient">e</span><span className="text-gradient">l</span><span className="text-gradient">k</span><span className="text-gradient">i</span><span className="text-gradient">n</span><span className="text-gradient"></span><span className='text-gradient1'>{'<'}{'/>'}</span></h1>
       </div> 
 
-      <div className='bg-white'>
+      <div className='bg-gradient-to-b from-slate-50 via-zinc-50 to-zinc-100'>
         <h1 className='mt-20 mb-40 font-rale'> <span className='text-gradient1'>{'<'}</span> <span className='text-gradient'>w</span><span className="text-gradient">e</span><span className="text-gradient">l</span><span className="text-gradient">k</span><span className="text-gradient">i</span><span className="text-gradient">n</span><span className="text-gradient"></span><span className='text-gradient1'>{'/>'}</span></h1>
       </div> 
 

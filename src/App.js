7540f205535c91775e61e29226b33b1c2078a1a1
kiitan-jsx <ajaladamilola09@protonmail.com>
2023-09-25 05:23:56 -0400

    logo

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
 
       <div className='bg-white'>
-        <h1 className='mt-20 mb-40 font-rale'> <span className='text-gradient'>w</span><span className="text-gradient">e</span><span className="text-gradient">l</span><span className="text-gradient">k</span><span className="text-gradient">i</span><span className="text-gradient">n</span><span className="text-gradient"></span></h1>
+        <h1 className='mt-40 mb-40 font-rale'> <span className='text-gradient'>w</span><span className="text-gradient">e</span><span className="text-gradient">l</span><span className="text-gradient">k</span><span className="text-gradient">i</span><span className="text-gradient">n</span><span className="text-gradient"></span></h1>
       </div> 
 
 

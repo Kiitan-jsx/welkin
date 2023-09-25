d415212eaa74201504d7dccef3adedd2d084d89b
kiitan-jsx <ajaladamilola09@protonmail.com>
2023-09-25 05:20:17 -0400

    logo

--- src/App.js
+++ src/App.js
@@ -5,21 +5,18 @@ function App() {
     <div className="App">
 
       <div className='bg-white'>
-        <h1 className='mt-20 mb-10 font-rale'> <span className='text-gradient'>w</span><span className="text-gradient">e</span><span className="text-gradient">l</span><span className="text-gradient">k</span><span className="text-gradient">i</span><span className="text-gradient">n</span><span className="text-gradient"></span></h1>
+        <h1 className='mt-20 mb-40 font-rale'> <span className='text-gradient'>w</span><span className="text-gradient">e</span><span className="text-gradient">l</span><span className="text-gradient">k</span><span className="text-gradient">i</span><span className="text-gradient">n</span><span className="text-gradient"></span></h1>
       </div> 
 
-      <hr style={{ border: '1px solid #000', margin: '10px 0' }}/>
 
       <div className='bg-white'>
-        <h1 className='mt-20 mb-10 font-rale'> <span className='text-gradient1'>{'<'}{'>'}</span> <span className='text-gradient'>w</span><span className="text-gradient">e</span><span className="text-gradient">l</span><span className="text-gradient">k</span><span className="text-gradient">i</span><span className="text-gradient">n</span><span className="text-gradient"></span><span className='text-gradient1'>{'<'}{'/>'}</span></h1>
+        <h1 className='mt-20 mb-40 font-rale'> <span className='text-gradient1'>{'<'}{'>'}</span> <span className='text-gradient'>w</span><span className="text-gradient">e</span><span className="text-gradient">l</span><span className="text-gradient">k</span><span className="text-gradient">i</span><span className="text-gradient">n</span><span className="text-gradient"></span><span className='text-gradient1'>{'<'}{'/>'}</span></h1>
       </div> 
 
-      <hr style={{ border: '1px solid #000', margin: '10px 0' }}/>
       <div className='bg-white'>
         <h1 className='mt-20 mb-10 font-rale'> <span className='text-gradient1'>{'<'}</span> <span className='text-gradient'>w</span><span className="text-gradient">e</span><span className="text-gradient">l</span><span className="text-gradient">k</span><span className="text-gradient">i</span><span className="text-gradient">n</span><span className="text-gradient"></span><span className='text-gradient1'>{'/>'}</span></h1>
       </div> 
 
-      <hr style={{ border: '1px solid #000', margin: '10px 0' }}/>
 
     </div>
   );
